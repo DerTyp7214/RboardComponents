1b363c439d30dff53a1aaef17ad1db20ab4b3308
DerTyp7214 <lengwenath1@gmail.com>
2022-09-15 01:23:16 +0200

    lib updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,9 +34,9 @@ android {
 
 dependencies {
     implementation(project(":colorutilsc"))
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.5.0")
-    implementation("com.google.android.material:material:1.6.1")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.8.0-alpha01")
     implementation("androidx.preference:preference-ktx:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
