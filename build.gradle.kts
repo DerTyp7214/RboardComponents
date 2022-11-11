eb2b751d2dc192ab277a723c64851c250be86840
DerBot7214 <lengwenath1@gmail.com>
2022-11-11 21:49:19 +0100

    gradle and signing update

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,8 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0-alpha02")
     implementation("androidx.preference:preference-ktx:1.2.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.20")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.20")
+    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.7.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.7.21")
 }
