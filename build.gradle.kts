b8cfc04c5579818e9e2289d1984bc08af1d6c160
DerBot7214 <lengwenath1@gmail.com>
2023-05-11 19:14:31 +0200

    gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,11 +44,10 @@ android {
 
 dependencies {
     implementation(project(":colorutilsc"))
-    implementation("androidx.core:core-ktx:1.10.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0-alpha01")
+    implementation("com.google.android.material:material:1.10.0-alpha02")
     implementation("androidx.preference:preference-ktx:1.2.0")
-    implementation("androidx.core:core-ktx:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
