e62862ef732924ee3f3ea56bc424f0b1f145354b
PahaAkos <paha.akos@gmail.com>
2023-10-25 22:42:16 +0200

    Update existing app themes

--- src/main/kotlin/de/dertyp7214/rboardcomponents/utils/ThemeUtils.kt
+++ src/main/kotlin/de/dertyp7214/rboardcomponents/utils/ThemeUtils.kt
@@ -23,7 +23,6 @@ import de.dertyp7214.rboardcomponents.core.preferences
 
 object ThemeUtils {
     val APP_THEMES = mapOf(
-        R.string.style_amoled to THEMES.AMOLED.name,
         R.string.style_apocyan to THEMES.APOCYAN.name,
         R.string.style_blue to THEMES.BLUE.name,
         R.string.style_brown_blue to THEMES.BROWN_BLUE.name,
@@ -38,7 +37,7 @@ object ThemeUtils {
         R.string.style_red to THEMES.RED.name,
         R.string.style_samoan_sun to THEMES.SAMOAN_SUN.name,
         R.string.style_yellow to THEMES.YELLOW.name,
-        R.string.style_yellow_purple to THEMES.YELLOW_PURPLE.name,
+        R.string.style_yellow_blue to THEMES.YELLOW_BLUE.name,
         R.string.style_default to THEMES.DEFAULT.name
     )
 
@@ -149,7 +148,6 @@ object ThemeUtils {
     @StyleRes
     fun getTheme(context: Context, style: String = getStyleName(context)): Int? {
         return when (style) {
-            THEMES.AMOLED.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_amoled
             THEMES.APOCYAN.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_apocyan
             THEMES.BLUE.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_blue
             THEMES.BROWN_BLUE.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_brown_blue
@@ -164,7 +162,7 @@ object ThemeUtils {
             THEMES.RED.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_red
             THEMES.SAMOAN_SUN.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_samoan_sun
             THEMES.YELLOW.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_yellow
-            THEMES.YELLOW_PURPLE.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_yellow_purple
+            THEMES.YELLOW_BLUE.name -> R.style.ThemeOverlay_RboardThemeManager_Colors_yellow_blue
             else -> null
         }
     }
@@ -187,5 +185,5 @@ object ThemeUtils {
 }
 
 enum class THEMES {
-    BLUE, GREEN, GREEN_BROWN, MONOCHROME, APOCYAN, AMOLED, LAVENDER_TONIC, BROWN_BLUE, PEACH_PEARL, RED, YELLOW, YELLOW_PURPLE, SAMOAN_SUN, ORANGE, PINK, LIME, DEFAULT
+    BLUE, GREEN, GREEN_BROWN, MONOCHROME, APOCYAN, LAVENDER_TONIC, BROWN_BLUE, PEACH_PEARL, RED, YELLOW, YELLOW_BLUE, SAMOAN_SUN, ORANGE, PINK, LIME, DEFAULT
 }
