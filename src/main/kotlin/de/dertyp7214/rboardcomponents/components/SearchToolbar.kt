12d2c31b5819d2ef61d304da5d80d48fba3316ac
PahaAkos <paha.akos@gmail.com>
2025-07-14 16:35:15 +0200

    Add new UI elements

--- src/main/kotlin/de/dertyp7214/rboardcomponents/components/SearchToolbar.kt
+++ src/main/kotlin/de/dertyp7214/rboardcomponents/components/SearchToolbar.kt
@@ -124,7 +124,7 @@ class SearchToolbar(
                     animator.addUpdateListener {
                         val ratio = it.animatedValue as Float
 
-                        setMargin(all = (margin * ratio).roundToInt())
+                        setMargin(0, (margin * ratio).roundToInt(),0, (margin * ratio).roundToInt())
                         radius = cornerRadius * ratio
                         setCardBackgroundColor(
                             ColorUtilsC.blendARGB(
